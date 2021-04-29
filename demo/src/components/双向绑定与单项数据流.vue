eb2ea0a163f3e2309b97950892801703ab286b62
huolun <liuzhe@cai-inc.com>
2021-04-29 18:53:05 +0800

    readme

---
+++ demo/src/components/双向绑定与单项数据流.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <!--  v-module 等同于 value + input事件-->
+    {{a}}<input type="text" v-model="a">
+    <br>
+    {{b}}<input type="text" :value="b" @input="handleInput">
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "v",
+  data() {
+    return{
+      a:'',
+      b:''
+    }
+  },
+  methods:{
+    handleInput(e){
+      this.b = e.target.value
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
