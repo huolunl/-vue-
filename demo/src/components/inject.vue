85432b9459a77555cd50cd2f0a7502286f51ba9e
huolun <liuzhe@cai-inc.com>
2021-05-11 20:37:23 +0800

    provideInject.vue

---
+++ demo/src/components/inject.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <span>{{p1}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+//  会向上级组件寻找p1，找到第一个就停止
+name: "inject",
+  inject: {
+    p1:{
+      from: "p1",
+      default: "p2"
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ demo/src/components/provideInject.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <inject></inject>
+  </div>
+</template>
+
+<script>
+import inject from "@/components/inject"
+export default {
+  name: "provideInject",
+  components:{
+    inject
+  },
+  provide: {
+    p1: "p1"
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
