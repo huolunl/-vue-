c03f96d3eabf1db41755d1a0620d702e563dd9cb
火轮 <liuzhe@cai-inc.com>
2021-10-25 10:27:18 +0800

    provideInject.vue

---
+++ demo/src/components/router.vue
@@ -0,0 +1,21 @@
+<template>
+
+</template>
+
+<script>
+// 作用  监听URL的变化并在变化前后执行相应的逻辑
+// 不同的URL对应不同的组件
+// 提供多种方式改变URL（url的改变不会刷新浏览器）
+
+// 挂载到vue实例上后，只需提供一个占位，来展示url匹配到的组件即可
+
+// 使用history模式可以消除#
+// mode:history
+export default {
+  name: "router"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ demo/src/components/vuex.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    {{count}}
+    <button @click="$store.commit('increment',2)">count++</button>
+    <button @click="$store.dispatch('increment',2)">sync count++</button>
+    <span>getter{{$store.getters.d}}</span>
+  </div>
+</template>
+
+<script>
+// vuex 可以作为一个全局变量，组件间共用
+export default {
+  name: "vuex",
+  computed: {
+    // 使用vuex时需要用计算属性视图才会刷新
+    count(){
+      return this.$store.state.count
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
